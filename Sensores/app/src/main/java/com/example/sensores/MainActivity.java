db52ea4144176d0c65de09f9887aa6e24f1b9ade
David <drodriguez2605@gamil.com>
2026-03-08 10:15:08 -0300

    uso del sensor de proximidad

---
+++ Sensores/app/src/main/java/com/example/sensores/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.sensores;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+TextView temVal;
+SensorManager sensorManager;
+Sensor sensor;
+SensorEventListener sensorEventListener;
+
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorproximidad();
+        }
+        private void iniciar(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    private void sensorproximidad(){
+        temVal = findViewById(R.id.lblSensorProximidad);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(sensor==null){
+            temVal.setText("No dispones del sensor de proximidad");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                double valor = sensorEvent.values[0];
+                temVal.setText("Proximidad " + valor);
+                int color = Color.BLACK;
+                if(valor<=4){
+                    color = Color.WHITE;
+                }
+
+                getWindow().getDecorView().setBackgroundColor(color);
+            }
+        };
+        }
+    }
