e9b9f8b95ba63dd5139b4955623cef658785b01a
David <drodriguez2605@gamil.com>
2026-03-14 13:34:41 -0300

    creacion de la UI la clase DB

--- amigoSqlite/app/src/main/java/com/example/amigosqlite/DB.java
+++ amigoSqlite/app/src/main/java/com/example/amigosqlite/DB.java
@@ -1,45 +1,77 @@
 package com.example.amigosqlite;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 public class DB extends SQLiteOpenHelper {
-    private static final String DataBase_Name="amigos";
-    private static final int DataBase_Version=1;
-    private static final String SQLDb="create table amigos (idAmigo integer primary key autoincrement, nombre TEXT, direccion TEXT,telefono TEXT, email TEXT, dui TEXT, urlfoto TEXT)";
+    private static final String DataBase_Name = "amigos";
+    private static final int DataBase_Version = 1;
+    private static final String SQLDb = "CREATE TABLE amigos (" +
+            "idAmigo INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "nombre TEXT, direccion TEXT, telefono TEXT, " +
+            "email TEXT, dui TEXT, urlFoto TEXT)";
 
-    public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DataBase_Name, factory, version);
+    public DB(@Nullable Context context, @Nullable SQLiteDatabase.CursorFactory factory) {
+        super(context, DataBase_Name, factory, DataBase_Version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        SQLiteDatabase.execSQL(SQLDb);
-
+        db.execSQL(SQLDb);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
     }
-    public String administrar_amigos(String accion, String[] datos){
+
+    public String administrar_amigos(String accion, String[] datos) {
         try {
             SQLiteDatabase db = getWritableDatabase();
-            String mensaje ="ok", sql ="";
-            switch (accion){
+            String mensaje = "ok", sql = "";
+
+            switch (accion) {
                 case "nuevo":
-                sql = "INSERT INTO amigos (nombre,dereccion,telefono,email,dui,urlfoto) VALUES('"+datos[1]+"','"+datos[2]+"','"+datos[3]+"','"+datos[4]+"','"+datos[5]+"','"+datos[6]+"')'";
-                break;
+                    sql = "INSERT INTO amigos (nombre, direccion, telefono, email, dui, urlFoto) VALUES(" +
+                            "'" + datos[1] + "'," +
+                            "'" + datos[2] + "'," +
+                            "'" + datos[3] + "'," +
+                            "'" + datos[4] + "'," +
+                            "'" + datos[5] + "'," +
+                            "'" + datos[6] + "'" +
+                            ")";
+                    break;
+
                 case "modificar":
-                sql ="'UPDATE amigos SET nombre='"+datos[1]+"',direccion='"+datos[2]+"',telefono='"+datos[3]
-                break;
-                case ="eliminar":
-                sql ="";
-                break;
+                    sql = "UPDATE amigos SET " +
+                            "nombre='" + datos[1] + "'," +
+                            "direccion='" + datos[2] + "'," +
+                            "telefono='" + datos[3] + "'," +
+                            "email='" + datos[4] + "'," +
+                            "dui='" + datos[5] + "'," +
+                            "urlFoto='" + datos[6] + "' " +
+                            "WHERE idAmigo='" + datos[0] + "'";
+                    break;
+
+                case "eliminar":
+                    sql = "DELETE FROM amigos WHERE idAmigo='" + datos[0] + "'";
+                    break;
             }
+
+            db.execSQL(sql);
+            db.close();
+            return mensaje;
+
+        } catch (Exception e) {
+            return e.getMessage();
         }
+    }
+
+    public Cursor lista_amigos() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT * FROM amigos", null);
     }
 }
