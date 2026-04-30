7596e0af57294b834b5bb71bdade41c31cc9ad4e
David <drodriguez2605@gamil.com>
2026-04-30 10:20:15 -0300

    Actualizacio de ip

--- TiendaRopa2/app/src/main/java/com/example/tiendaropa2/CouchDBHelper.java
+++ TiendaRopa2/app/src/main/java/com/example/tiendaropa2/CouchDBHelper.java
@@ -2,7 +2,7 @@ package com.example.tiendaropa2;
 
 public class CouchDBHelper {
 
-    private static final String BASE_URL = "http://192.168.1.5:5984";
+    private static final String BASE_URL = "http://192.168.80.194:5984";
     private static final String DATABASE = "david";
     private static final String USUARIO = "david123";
     private static final String PASSWORD = "12345";
